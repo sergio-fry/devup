6bb995fdadddf93f0132199d01a0bd871282f64f
Sergei O. Udalov <sergei.udalov@gmail.com>
2020-06-03 06:56:42 +0300

    service

--- lib/devup/service.rb
+++ lib/devup/service.rb
@@ -1,4 +1,21 @@
+require "ostruct"
+
 module Devup
   class Service
+    attr_reader :compose, :name
+
+    def initialize(compose, name)
+      @compose = compose
+      @name = name
+    end
+
+    def ports
+      compose.service_ports(name).map { |el| el.to_s.split(":")[-1] }.map do |from|
+        OpenStruct.new(
+          from: from,
+          to: compose.command("port #{name} #{from}").split(":")[-1].strip
+        )
+      end
+    end
   end
 end
--- spec/devup/service_spec.rb
+++ spec/devup/service_spec.rb
@@ -2,5 +2,22 @@ require "devup/service"
 
 module Devup
   RSpec.describe Service do
+    let(:service) { described_class.new compose, "postgres" }
+
+    let(:compose) do
+      double(:compose, service_ports: ["5432"], command: "0.0.0.0:32772")
+    end
+
+    describe "#ports" do
+      subject(:ports) { service.ports }
+
+      it { is_expected.to be_an Array }
+
+      describe "port" do
+        subject { ports.first }
+        it { expect(subject.from).to eq "5432" }
+        it { expect(subject.to).to eq "32772" }
+      end
+    end
   end
 end

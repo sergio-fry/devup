a697ef0f2c0f22c33b8841dcde6cff69832132a2
Sergei O. Udalov <sergei.udalov@gmail.com>
2020-06-03 06:58:19 +0300

    simplecov

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gemspec
 gem "rake", "~> 12.0"
 gem "rspec", "~> 3.0"
 gem "standard"
+gem "simplecov", require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require "bundler/setup"
+require "simplecov"
+SimpleCov.start
+
 require "devup"
 
 RSpec.configure do |config|

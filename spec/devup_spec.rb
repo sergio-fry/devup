32a369ec92d35ddc194c4867c76c8c6083fff941
Sergei O. Udalov <sergei.udalov@gmail.com>
2020-06-09 07:11:04 +0300

    docker-compose.devup.yml config resolve #14

--- spec/devup_spec.rb
+++ spec/devup_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Devup do
   let(:docker_compose_path) { Root.join("spec/dummy/docker-compose.yml") }
 
   let(:devup) { Devup::Environment.new pwd: Root.join("spec/dummy"), logger: logger }
-  let(:logger) { double(:logger, info: nil) }
+  let(:logger) { double(:logger, info: nil, error: nil) }
 
   it "works" do
     devup.up

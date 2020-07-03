891df78024065fbe292469b37cbd8910522e4e1d
Sergei O. Udalov <sergei.udalov@gmail.com>
2020-07-03 17:28:54 +0300

    Revert "GH-23 run devup each spring"
    This reverts commit 60600d99138ab4486e171ed98b94df043ecbebe8.

--- lib/devup/boot.rb
+++ lib/devup/boot.rb
@@ -3,18 +3,13 @@ if ENV.fetch("DEVUP_ENABLED", "true") == "true"
     pwd: `pwd`,
     logger: Devup::Logger.build(ENV.fetch("DEVUP_LOG_LEVEL", "info"))
   )
+  devup.up
 
   begin
     require "spring/commands"
 
-    Spring.after_fork do
-      devup.up
-    end
-
-    Spring.watch devup.root.join("docker-compose.devup.yml")
+    Spring.watch devup.root.join("docker-compose.yml")
   rescue LoadError, ArgumentError
     # Spring is not available
-
-    devup.up
   end
 end

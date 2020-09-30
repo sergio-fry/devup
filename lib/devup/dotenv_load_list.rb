f9da1f999b7882e185b9d85136e58a4265d7a8e8
Sergei O. Udalov <sergei.udalov@gmail.com>
2020-09-30 07:28:10 +0300

    GH-35 patch_02 07:28

--- lib/devup/dotenv_load_list.rb
+++ lib/devup/dotenv_load_list.rb
@@ -1,10 +1,16 @@
 module Devup
   class DotenvLoadList
+    def initialize(env: nil)
+      @env = env.to_sym unless env.nil?
+    end
+
     def to_a
       list = []
 
-      list << ".env.local"
+      list << ".env.#{@env}.local" if env_defined?
+      list << ".env.local" unless test?
       list << ".env.services"
+      list << ".env.#{@env}" if env_defined?
       list << ".env"
 
       list
@@ -16,6 +22,14 @@ module Devup
 
     def index(val)
       to_a.index val
+    end
+
+    def test?
+      @env == :test
+    end
+
+    def env_defined?
+      !@env.nil?
     end
   end
 end
--- spec/devup/dotenv_load_list_spec.rb
+++ spec/devup/dotenv_load_list_spec.rb
@@ -10,7 +10,8 @@ require "devup/dotenv_load_list"
 
 module Devup
   RSpec.describe DotenvLoadList do
-    subject { DotenvLoadList.new }
+    subject { DotenvLoadList.new env: app_env }
+    let(:app_env) { nil }
 
     it { is_expected.to include ".env.services" }
     it { is_expected.to include ".env" }
@@ -23,7 +24,15 @@ module Devup
       let(:app_env) { :development }
 
       it { is_expected.to include ".env.development.local" }
+      it { is_expected.to include ".env.development" }
       it { expect(".env.local").to have_less_priority_than(".env.development.local") }
+      it { expect(".env.development").to have_less_priority_than(".env.services") }
+      it { expect(".env.development").to have_less_priority_than(".env.development.local") }
+    end
+
+    context "when env is test" do
+      let(:app_env) { :test }
+      # it { is_expected.not_to include ".env.local" }
     end
   end
 end

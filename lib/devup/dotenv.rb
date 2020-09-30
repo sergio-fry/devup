d7fb9c93b823fa5bd4c4223c773559324b3d47a8
Sergei O. Udalov <sergei.udalov@gmail.com>
2020-09-30 06:33:24 +0300

    GH-35 rails updated

--- lib/devup/dotenv.rb
+++ lib/devup/dotenv.rb
@@ -1,2 +1,8 @@
 require "dotenv"
-Dotenv.load ".env.services"
+
+Dotenv.load(
+  ".env.development.local", ".env.test.local", ".env.production.local", ".env.local",
+  ".env.services",
+  ".env.development", ".env.test", ".env.production",
+  ".env"
+)

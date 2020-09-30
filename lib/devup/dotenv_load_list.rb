248bda2b9c9f3d76961761c7fab5981a110c80bc
Sergei O. Udalov <sergei.udalov@gmail.com>
2020-09-30 07:16:38 +0300

    GH-35 patch_01 07:16

--- lib/devup/dotenv_load_list.rb
+++ lib/devup/dotenv_load_list.rb
@@ -1,7 +1,21 @@
 module Devup
   class DotenvLoadList
     def to_a
-      [".env.services"]
+      list = []
+
+      list << ".env.local"
+      list << ".env.services"
+      list << ".env"
+
+      list
+    end
+
+    def include?(val)
+      to_a.include? val
+    end
+
+    def index(val)
+      to_a.index val
     end
   end
 end
--- spec/devup/dotenv_load_list_spec.rb
+++ spec/devup/dotenv_load_list_spec.rb
@@ -1,9 +1,29 @@
+require "rspec/expectations"
+
+RSpec::Matchers.define :have_less_priority_than do |expected|
+  match do |actual|
+    subject.index(actual) > subject.index(expected)
+  end
+end
+
 require "devup/dotenv_load_list"
 
 module Devup
   RSpec.describe DotenvLoadList do
-    subject { DotenvLoadList.new.to_a }
+    subject { DotenvLoadList.new }
 
     it { is_expected.to include ".env.services" }
+    it { is_expected.to include ".env" }
+    it { is_expected.to include ".env.local" }
+
+    it { expect(".env").to have_less_priority_than(".env.services") }
+    it { expect(".env.services").to have_less_priority_than(".env.local") }
+
+    context "when env is development" do
+      let(:app_env) { :development }
+
+      it { is_expected.to include ".env.development.local" }
+      it { expect(".env.local").to have_less_priority_than(".env.development.local") }
+    end
   end
 end
